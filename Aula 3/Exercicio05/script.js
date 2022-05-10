2f6ef86a2cad9cd19e964068aaec1a6ea7b314dd
Luiz F3lipe <luiz.felipe.moraes.lima.1@gmail.com>
2022-05-10 19:50:51 -0300

    Feat: Exercicio05 add

---
+++ Aula 3/Exercicio05/script.js
@@ -0,0 +1,9 @@
+function media () {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let n3 = parseInt(document.getElementById("n3").value);
+    let n4 = parseInt(document.getElementById("n4").value);
+    let n5 = parseInt(document.getElementById("n5").value);
+    let resposta =  document.write(n1 + n2 + n3 + n4 + n5);
+    console.log(resposta.value)
+  }
